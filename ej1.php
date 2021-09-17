191d57e60335bb56879d9ab22c0b391a1a870a8b
DanielFerrera <90683693+DanielFerrera@users.noreply.github.com>
2021-09-17 13:30:59 +0200

    Add files via upload

---
+++ ej1.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD> Primeros scripts PHP </HEAD>
+<H1> Uso de la funci&oacute;n "echo" </H1>
+<BODY>
+<?php
+           echo "Hola Mundo. Mi primer script en PHP. ";
+           echo "Esta es la forma de escribir en php";
+
+?>
+</BODY>
+</HTML>
