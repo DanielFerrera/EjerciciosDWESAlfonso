e3d9ed93334bdcc6e549e305197620cb597e8bff
DanielFerrera <90683693+DanielFerrera@users.noreply.github.com>
2021-09-23 11:03:29 +0200

    Add files via upload

---
+++ ej3s.php
@@ -0,0 +1,98 @@
+<HTML>
+<HEAD> <TITLE>  EJ2-Direccion Red – Broadcast y Rango</TITLE> </HEAD>
+
+<BODY>
+<?php
+
+echo "<h2>Daniel Ferrera</h2>";
+
+$ip="192.168.16.100/16";
+echo "<b>IP= $ip </b>";
+$ip20= explode('/',$ip);
+$primerNumero=$ip20[0];
+$mascara=$ip20[1];
+$bits=32;
+$bitsip=$bits-$mascara;
+$segundoNumero= explode('.',$primerNumero);
+$unobinario=$segundoNumero[0];
+$dosbinario=$segundoNumero[1];
+$tresbinario=$segundoNumero[2];
+$cuatrobinario=$segundoNumero[3];
+
+
+//$mascara=substr($ip,14,16);
+//echo "<br>Máscara: $mascara";
+
+//echo "<br>IP en binario: ";
+//$ip1=substr($ip,0,3);
+//echo decbin($ip1);
+//echo ".";
+
+//$ip2=substr($ip,4,6);
+//echo decbin($ip2);
+//echo ".";
+
+//$ip3=substr($ip,7,8);
+//echo decbin($ip3);
+//echo ".";
+
+//$ip4=substr($ip,10,13);
+//echo decbin($ip4);
+
+echo "</br>Ip sin mascara: $primerNumero</br>";
+echo "Ip sin puntos: $unobinario$dosbinario$tresbinario$cuatrobinario</br>";
+$primera= sprintf ("%b",$unobinario);
+$segunda=sprintf ("%b",$dosbinario);
+$tercera=sprintf ("%b",$tresbinario);
+$cuarta=sprintf ("%b",$cuatrobinario);
+
+$primera2=str_pad($primera,8,"0",STR_PAD_LEFT);
+$segunda2=str_pad($segunda,8,"0",STR_PAD_LEFT);
+$tercera2=str_pad($tercera,8,"0",STR_PAD_LEFT);
+$cuarta2=str_pad($cuarta,8,"0",STR_PAD_LEFT);
+
+
+$todojunto=$primera2.$segunda2.$tercera2.$cuarta2;
+echo "IP en binario todo junto:$todojunto </br>";
+echo "Las IPs tienen 32 bits por lo que se le han de restar los bits de la máscara, que en este caso son: ";
+echo "$bitsip</br>";
+echo "string</br>";
+
+//Direccion de red
+for ($i=1; $i <=$bitsip; $i++) {
+$todojunto[strlen($todojunto)-$i]="0";
+}
+
+echo "Ahora tenemos la Direccion de red en binario: ".$todojunto;
+echo "</br>";
+$primerocteto=substr($todojunto,0,8);
+$segundoocteto=substr($todojunto,8,8);
+$tercerocteto=substr($todojunto,16,8);
+$cuartoocteto=substr($todojunto,24,8);
+echo "Direccion de red: ";
+echo bindec($primerocteto).".";
+echo bindec($segundoocteto).".";
+echo bindec($tercerocteto).".";
+echo bindec($cuartoocteto);
+
+//Broadcast
+for ($i=1; $i <=$bitsip; $i++) {
+$todojunto[strlen($todojunto)-$i]="1";
+}
+echo "</br>Ahora tenemos el Broadcast: ".$todojunto;
+echo "</br>";
+
+$primerocteto=substr($todojunto,0,8);
+$segundoocteto=substr($todojunto,8,8);
+$tercerocteto=substr($todojunto,16,8);
+$cuartoocteto=substr($todojunto,24,8);
+echo "Broadcast: ";
+echo bindec($primerocteto).".";
+echo bindec($segundoocteto).".";
+echo bindec($tercerocteto).".";
+echo bindec($cuartoocteto);
+
+
+?>
+</BODY>
+</HTML>
