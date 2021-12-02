babe856380d8314b7e218005ce0d7b51061df0b1
DanielFerrera <90683693+DanielFerrera@users.noreply.github.com>
2021-12-02 11:06:54 +0100

    Add files via upload

--- Bases de Datos/ej2-AltaEmpleado/empaltaemp.php
+++ Bases de Datos/ej2-AltaEmpleado/empaltaemp.php
@@ -6,7 +6,7 @@
 </head>
 <body style="background-color:#85C9DC;">
   <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <table border="1" rules="none">
+    <table border="1" rules="none">
       <h1 style="color:black;">Alta Empleado</h1>
       <tr>
         <td> DNI:</td> <td><input type='text' name='dni' placeholder='DNI'></td>
@@ -17,36 +17,62 @@
         <td>Apellidos:</td><td> <input type='text' name='apellidos' placeholder='Apellido1 Apellido2'></td>
       </tr>
       <tr>
-        <td>Fecha de nacimiento:</td><td> <input type='text' name='fecha' placeholder='Fecha de nacimiento'></td>
+        <td>Fecha de nacimiento:</td><td> <input type='text' name='fecha' placeholder='Año/Mes/Dia'></td>
       </tr>
       <tr>
         <td>Salario:</td><td> <input type='text' name='salario' placeholder='Salario'></td>
       </tr>
       <tr>
-        <td>Departamento:</td><td> <select name="OS">
-          <option value="1">Windows Vista</option>
-          <option value="2">Windows 7</option>
-        </select></td>
+        <td>Departamento:</td><td>
+          <select name="nombredept">
+            <?php
+            //Fichero de funciones
+            include "./funciones.php";
+
+            //Parametros recibidos
+            $servername="localhost"; $username="root"; $password="rootroot"; $dbname="empleadosnnprofe";
+
+            //if
+            if ($_SERVER["REQUEST_METHOD"]=="POST") {
+              $dni=$_POST['dni']; $nombre=$_POST['nombre']; $apellidos=$_POST['apellidos'];
+              $fecha=$_POST['fecha'];$salario=$_POST['salario']; $nombredept=$_POST['nombredept'];
+
+              //Comprobacion de parametros
+              revisarparamentros($dni, $nombre, $apellidos, $fecha, $salario);
+            }
+
+            //Creamos la conexion
+            $conexion=crearconexionpdo($servername, $username, $password, $dbname);
+
+            //Lógica de Negocio
+            $arraydepartamentos=mostrardepartamentospdo($conexion);
+            // var_dump($arraydepartamentos);
+
+            foreach ($arraydepartamentos as $key) {
+              ?> <option> <?php echo $key ?> </option>
+              <?php
+            }
+            ?>
+          </select>
+        </td>
       </tr>
     </table></br>
-    <input type="submit" value="Enviar">
+    <input type="submit" value="Enviar" name="enviar">
     <input type="reset" value="Borrar">
   </form>
 </body>
 </html>
-
 <?php
-//Fichero de funciones
-include "./funciones.php";
+//Si se pulsa el boton de enviar
+if (isset($_POST['enviar'])) {
+  // echo "</br>Datos introducidos: </br>$dni $nombre $apellidos $fecha $salario $nombredept";
 
-//Parametros recibidos
-$servername="localhost"; $username="root"; $password="rootroot"; $dbname="empleadosnnprofe";
-$dni=$_POST['dni']; $nombre=$_POST['nombre']; $apellidos=$_POST['apellidos'];
-$fecha=$_POST['fecha'];$salario=$_POST['salario'];
-//Comprobacion de parametros
-revisarparamentros($dni, $nombre, $apellidos, $fecha, $salario);
+  //Metemos al usuario dentro de la tabla de Empleado
+  introducirempleado($dni,$nombre,$apellidos,$fecha,$salario,$conexion);
+  $fecha=date('Y-m-d');
+  echo "</br>";
+  //Unimos tablas, empleado y departamento en emple_depart
+  introduciremple_depart($dni,$fecha,$nombredept,$conexion);
 
-echo "a";
-
-
+}
 ?>
--- Bases de Datos/ej2-AltaEmpleado/funciones.php
+++ Bases de Datos/ej2-AltaEmpleado/funciones.php
@@ -1,10 +1,11 @@
 <?php
-function revisarparamentros($servername, $username, $password, $dbname){
+function revisarparamentros($dni, $nombre, $apellidos, $fecha, $salario){
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $servername = test_input($servername);
-  $username = test_input($username);
-  $password = test_input($password);
-  $dbname = test_input($dbname);
+  $dni = test_input($dni);
+  $nombre = test_input($nombre);
+  $apellidos = test_input($apellidos);
+  $fecha = test_input($fecha);
+  $salario = test_input($salario);
 }
 }
 
@@ -20,8 +21,8 @@ function crearconexionpdo($servername, $username, $password, $dbname){
   try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo "Servidor: <b>$servername</b>, Usuario: <b>$username</b>, Base de datos en uso: <b>$dbname</b>";
-    echo "<br>Conectado correctamente";
+    // echo "Servidor: <b>$servername</b>, Usuario: <b>$username</b>, Base de datos en uso: <b>$dbname</b>";
+    // echo "<br>Conectado correctamente";
     return $conn;
   }
   catch(PDOException $e)
@@ -72,23 +73,68 @@ function creardepartamentopdo($nombredept,$conexion){
     $conn = null;
 }
 
-function mostrardepartamentospdo($nombredept,$contadordepartamentos,$conexion){
+function mostrardepartamentospdo($conexion){
+  $arraydepartamentos=array();
   try {
     $stmt = $conexion->prepare("SELECT cod_dpto,nombre_dpto FROM departamento");
     $stmt->execute();
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    echo "<br><br><b><u>DEPARTAMENTOS</u></b><br>";
     foreach($stmt->fetchAll() as $row) {
-      echo "Cod_dpto: <b>" . $row["cod_dpto"]."</b> Nombre: <b>".$row["nombre_dpto"]."</b><br>";
+      // echo "Cod_dpto: <b>" . $row["cod_dpto"]."</b> Nombre: <b>".$row["nombre_dpto"]."</b><br>";
+      array_push($arraydepartamentos,$row["nombre_dpto"]);
     }
+    // var_dump($arraydepartamentos);
   }
   catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
   }
-  echo "<br>Hay <b>$contadordepartamentos departamentos</b>";
+  return $arraydepartamentos;
 }
 
 
+function introducirempleado($dni,$nombre,$apellidos,$fecha,$salario,$conexion){
+  try {
+      $stmt = $conexion->prepare("INSERT INTO empleado VALUES ('$dni','$nombre','$apellidos','$fecha','$salario')");
+      $stmt->execute();
+      echo "Nuevo empleado creado correctamente";
+      }
+  catch(PDOException $e)
+      {
+      echo "Error: " . $e->getMessage();
+      }
+}
+
+function introduciremple_depart($dni,$fecha,$nombredept,$conexion){
+// echo "$dni,$fecha,$nombredept";
+try {
+    $stmt = $conexion->prepare("SELECT cod_dpto FROM departamento WHERE nombre_dpto='$nombredept'");
+    $stmt->execute();
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach($stmt->fetchAll() as $row) {
+      // echo "Cod_dpto: " . $row["cod_dpto"]."<br>";
+      //AL IGUALAR OBTENGO EL CODIGO DEL DEPARTAMENTO PARA HACER EL INSERT
+      $codigodepartamento=$row["cod_dpto"];
+      // echo $codigodepartamento;
+    }
+    }
+catch(PDOException $e)
+    {
+    echo "Error: " . $e->getMessage();
+    }
+
+//Insert de empleado en emple_depart
+echo "$fecha";
+try {
+    $stmt = $conexion->prepare("INSERT INTO emple_depart (dni,cod_dpto,fecha_ini) VALUES ('$dni','$codigodepartamento','$fecha')");
+    $stmt->execute();
+    echo "</br>Empleado asociado en Emple_depart";
+    }
+catch(PDOException $e)
+    {
+    echo "Error: " . $e->getMessage();
+    }
+
+}
 
 
 ?>
