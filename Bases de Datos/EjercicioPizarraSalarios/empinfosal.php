fea1417865f6e11e213c8684463b5f716519f56a
DanielFerrera <90683693+DanielFerrera@users.noreply.github.com>
2021-12-14 10:12:30 +0100

    Add files via upload

---
+++ Bases de Datos/EjercicioPizarraSalarios/empinfosal.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang='en' dir='ltr'>
+<head>
+  <meta charset='utf-8'>
+  <title>Empresa</title>
+</head>
+<body style="background-color:#85C9DC;">
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <table border="1" rules="none">
+      <h1 style="color:black;">Lista Empleados + Salario</h1>
+      Selecciona un departamento y se mostrarán sus empleados, su salario y su suma total:</br>
+        <tr>
+          <td>Departamento: </td>
+          <td>
+            <select name="nombredept">
+              <?php
+              //Fichero de funciones
+              include "./funciones.php";
+              //Parametros
+              $servername="localhost"; $username="root"; $password="rootroot"; $dbname="empleadosnn";
+
+              //Creamos la conexion
+              $conexion=crearconexionpdo($servername, $username, $password, $dbname);
+
+              //Revisar Parametros
+              if ($_SERVER["REQUEST_METHOD"]=="POST") {
+                 $nombredept=$_POST['nombredept'];
+                //Comprobacion de parametros
+                 revisarparamentros($nombredept);
+              }
+              //Lógica de Negocio
+              $arraydepartamentos=mostrardepartamentospdo($conexion);
+              var_dump($arraydepartamentos);
+
+              foreach ($arraydepartamentos as $key) {
+                ?> <option> <?php echo $key ?> </option>
+                <?php
+              }
+              ?>
+            </select>
+          </td>
+        </tr>
+      </table></br>
+      <input type="submit" value="Enviar" name="enviar">
+      <input type="reset" value="Borrar">
+    </form>
+  </body>
+  </html>
+  <?php
+  // Si se pulsa el boton de enviar
+  if (isset($_POST['enviar'])) {
+    // echo "</br>$nombredept";
+    mostrarsalarioempleadosdepartamento($nombredept,$conexion);
+  }
+
+  $conexion=null;
+  ?>
---
+++ Bases de Datos/EjercicioPizarraSalarios/funciones.php
@@ -0,0 +1,187 @@
+<?php
+function revisarparamentros($dni){
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $dni = test_input($dni);
+}
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+function crearconexionpdo($servername, $username, $password, $dbname){
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    // echo "Servidor: <b>$servername</b>, Usuario: <b>$username</b>, Base de datos en uso: <b>$dbname</b>";
+    // echo "<br>Conectado correctamente";
+    return $conn;
+  }
+  catch(PDOException $e)
+  {
+    echo "Conexion fallida: " . $e->getMessage();
+  }
+}
+
+function mostrardepartamentospdo($conexion){
+  $arraydepartamentos=array();
+  try {
+    $stmt = $conexion->prepare("SELECT cod_dpto,nombre_dpto FROM departamento");
+    $stmt->execute();
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach($stmt->fetchAll() as $row) {
+      // echo "Cod_dpto: <b>" . $row["cod_dpto"]."</b> Nombre: <b>".$row["nombre_dpto"]."</b><br>";
+      array_push($arraydepartamentos,$row["nombre_dpto"]);
+    }
+    // var_dump($arraydepartamentos);
+  }
+  catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+  }
+  return $arraydepartamentos;
+}
+
+function mostrarDNItablaempleado($conexion){
+  $arraydnis=array();
+  try {
+    $stmt = $conexion->prepare("SELECT dni FROM empleado");
+    $stmt->execute();
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach($stmt->fetchAll() as $row) {
+      // echo "Cod_dpto: <b>" . $row["cod_dpto"]."</b> Nombre: <b>".$row["nombre_dpto"]."</b><br>";
+      array_push($arraydnis,$row["dni"]);
+    }
+    // var_dump($arraydepartamentos);
+  }
+  catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+  }
+  return $arraydnis;
+}
+
+
+function introduciremple_depart($dni,$fecha,$nombredept,$conexion){
+try {
+    $stmt = $conexion->prepare("SELECT cod_dpto FROM departamento WHERE nombre_dpto='$nombredept'");
+    $stmt->execute();
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach($stmt->fetchAll() as $row) {
+      // echo "Cod_dpto: " . $row["cod_dpto"]."<br>";
+      //AL IGUALAR OBTENGO EL CODIGO DEL DEPARTAMENTO PARA HACER EL INSERT
+      $codigodepartamento=$row["cod_dpto"];
+      // echo $codigodepartamento;
+    }
+    }
+catch(PDOException $e)
+    {
+    echo "Error: " . $e->getMessage();
+    }
+
+//Insert de empleado en emple_depart
+echo "$fecha";
+try {
+    $stmt = $conexion->prepare("INSERT INTO emple_depart (dni,cod_dpto,fecha_ini) VALUES ('$dni','$codigodepartamento','$fecha')");
+    $stmt->execute();
+    echo "</br>Empleado asociado en Emple_depart";
+    }
+catch(PDOException $e)
+    {
+    echo "Error: " . $e->getMessage();
+    }
+
+}
+
+function actualizartablaemple_departfechafin($dni,$nombredept,$conexion){
+  try {
+      $stmt = $conexion->prepare("SELECT cod_dpto FROM departamento WHERE nombre_dpto='$nombredept'");
+      $stmt->execute();
+      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      foreach($stmt->fetchAll() as $row) {
+        // echo "Cod_dpto: " . $row["cod_dpto"]."<br>";
+        //AL IGUALAR OBTENGO EL CODIGO DEL DEPARTAMENTO PARA HACER EL INSERT
+        $codigodepartamento=$row["cod_dpto"];
+        // echo $codigodepartamento;
+      }
+      }
+  catch(PDOException $e)
+      {
+      echo "Error: " . $e->getMessage();
+      }
+    //update
+    $fechafin=date('Y-m-d');
+    echo "</br>";
+    try {
+        $stmt = $conexion->prepare("UPDATE emple_depart set fecha_fin='$fechafin' where dni='$dni'");
+        $stmt->execute();
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        }
+    catch(PDOException $e)
+        {
+        echo "Error: " . $e->getMessage();
+        }
+        $fechainicio=$fechafin;
+        try {
+          $stmt = $conexion->prepare("INSERT INTO emple_depart (dni,cod_dpto,fecha_ini) VALUES ('$dni','$codigodepartamento','$fechainicio')");
+            $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            }
+        catch(PDOException $e)
+            {
+            echo "Error: " . $e->getMessage();
+            }
+}
+
+function mostrarsalarioempleadosdepartamento($nombredept,$conexion){
+  //saco el codigo del departamento introducido
+ $contadorsalario=0;
+  try {
+    $stmt = $conexion->prepare("SELECT cod_dpto FROM departamento WHERE nombre_dpto='$nombredept'");
+      $stmt->execute();
+      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      foreach($stmt->fetchAll() as $row) {
+        // echo "Cod_dpto: " . $row["cod_dpto"]."<br>";
+        //obtengo el codigo para filtrar por codigo por que es la primary key
+        $codigodepartamento=$row["cod_dpto"];
+        // echo $codigodepartamento;
+      }
+      }
+  catch(PDOException $e)
+      {
+      echo "Error: " . $e->getMessage();
+      }
+      //busco todos los dnis que tengan ese codigo_dpto
+      try {
+        $stmt = $conexion->prepare("SELECT dni FROM emple_depart WHERE cod_dpto='$codigodepartamento'");
+          $stmt->execute();
+          $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+          foreach($stmt->fetchAll() as $row) {
+            // echo "Cod_dpto: " . $row["cod_dpto"]."<br>";
+            //obtengo el codigo para filtrar por codigo por que es la primary key
+            $dni=$row["dni"];
+            $stmt1 = $conexion->prepare("SELECT nombre,apellidos,salario FROM empleado WHERE dni='$dni'");
+            $stmt1->execute();
+            $result1 = $stmt1->setFetchMode(PDO::FETCH_ASSOC);
+            // echo "Empleados de <h3><b>$nombredept</b></h3>";
+            echo "<ul>";
+            foreach($stmt1->fetchAll() as $row) {
+              $nombre=$row["nombre"];
+              $apellidos=$row["apellidos"];
+              $salario=$row["salario"];
+              $contadorsalario=$salario+$contadorsalario;
+              echo "<li><b>Nombre:</b> $nombre<b> Apellidos: </b>$apellidos <b>Salario</b> $salario</li>";
+            }
+            echo "</ul>";
+          }
+          echo "<b>Salario total</b> de $nombredept: $contadorsalario";
+
+          }
+      catch(PDOException $e)
+          {
+          echo "Error: " . $e->getMessage();
+          }
+}
+
+?>
